f69df4498f8ae52b07fafe242cc941931218ab32
Karim <kacuna@buhoms.com>
2025-02-04 09:41:02 -0600

    secrets

--- app.py
+++ app.py
@@ -34,46 +34,7 @@ except locale.Error:
 # --------------------------------------------------------------------------------
 # CREDENCIALES DE SERVICIO
 # --------------------------------------------------------------------------------
-SERVICE_ACCOUNT_INFO = {
-    "type": "service_account",
-    "project_id": "buho-api-2024-sheets",
-    "private_key_id": "49baaf5d4716afe832bb5b215ee5d783b03a5f94",
-    "private_key": """-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDFlko5MebPggbD
-cxaiGkUUKU3bqZnlVE+vQNtOG69EuBJzUvpwQpF1TtYUnYNNyLxyZbv2kJoiywNJ
-n+UaWf0tw8cPVLbKwiwPF6fKntbR42xlxiQAWspBIVVlUBKNNIsZ2R9NLavc+NLH
-wQGVw8j5t3mcugASikV0PiGeMCjuLSEzOPyic4xjmTcRqeSaQ4oBOODeUkIeX6AV
-ahP5lPQDpC/wvD/ZTR6U/0dStc3NJQ6he+71kXXpum9pN1C4NYAjMMAL6R/upqd3
-xPxZsPwHS+pMs7cIGdjDUIIGBWMQJhoCnKqznIbxHEX7HtBv1R1oLrIIMoeFSDbX
-AvUQQMlNAgMBAAECggEAEVhudyNplP1f7SAJFF10g37VwisbIr36SdMKfYqiIgj7
-u6qE6D57yP3M/t3OBVHSM0O5kr1ifpvuU7QI931/Y5lce/zOmDGgDwofVYMIrj/G
-CBAzIGHYAAw2VDDJlCJQ9Mmx/QM9o2YnkNghdL2NgtiHwUm10GrZiokax+mH6lKd
-wTChqYPYtKttq9mREtoCGTqzPuqVR8vCdnKqAohddDfgaARNixay/PyrmqSiiWGp
-bdtZleRjSL4/F7xri+WKq30NliALHBd8jzSlkSvzHLgeNR+wyRUf/GpvMZBjFG8y
-B8ytXmy74h/yDzAYVj2tmgzywqiHlfLDBLVosTo1mQKBgQD2SGWcXVPp+Z0UOhjs
-o52Fd4JsMK4im83D6mdxprdOO3FUBFC44HRrMBqjWqHMBHRNgHFyPN10APhSBRe8
-MVxzWhXpAIM3K+5vB6hQr1PXN22Kp7bs9HGhF4vw8yWFMzUa44HlPKdVT2M6l5gn
-vWe9KktbatM8pkytplnl/V8oWQKBgQDNYgmAaYssQKnyZSeru7s701SFF6O8cZqJ
-VuliVwgkdcmTGTUpLZYei3QX7KHD3cAxKNZzdJdYA/G+nTkY98a5uzkd2liPmDW/
-7sBRH30/H8vpKVu0YjhFqdBDsblFRRtWLebNjvs7UOcDLFfnMEkByZ3BC7utxANm
-84Z6cvcKFQKBgHf4um7aU8dVjjxNNNkJtvFOT11OtXUseqbmZ+/IK+FTOZiY5Y25
-4VxZuZA71TdiMBmU6S6iEaqx0kV6L57AWO3kQ2oWktTsdKDnlQmA7xGW8aiqnIR/
-a17y7nu4pl1lnYf0rdEyo7z+CDOBp2Asdv2CPeVRe4c+53lr4L0VmSY5AoGAESBg
-vHWQpnMJ+O2YfkicV2PLA4IyJC+w/EzkD1BEnI257mtGtJVZlFh6qNgRsTyXn0HR
-iDUrvaouiX+g2EUpLCnBnIytn+PIb6XgIaOnlRD4twu82vDp0l1TwaFbWrxliC0x
-tuh6aLrZWLlk5yFupRiD8CojT10uD3K1PxbBJPUCgYEA4iyM3F1LzPKl4lXsBKZD
-/DC8P/mJyEONmDVGMb/T+/V4dbW327vMJP2tikLhIqAseN2/0kkd5obtnHAfUQvy
-vY8iNvaf6oA0sXuCetFH6cGJoBLK6RYOOFTnwVAa7ZI6nLKVN/FHOREs4xOWYiyP
-dNLv9ywuT9km02+2A/aqDDc=
------END PRIVATE KEY-----\n""",
-    "client_email": "google-api@buho-api-2024-sheets.iam.gserviceaccount.com",
-    "client_id": "105531071438636619321",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/google-api%40buho-api-2024-sheets.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-}
+SERVICE_ACCOUNT_INFO = st.secrets["Google"]["SERVICE_ACCOUNT_INFO"]
 
 # --------------------------------------------------------------------------------
 # CONFIGURACIÓN DE STREAMLIT
